0418750cfaf85f86b641744d77eeaf0421bb8475
Anna <kochurovaav@gmail.com>
2023-03-22 14:24:59 +0200

    Task08 is done

---
+++ Task08/Program.cs
@@ -0,0 +1,14 @@
+﻿Person tom = new Person();
+
+class Person 
+{
+    public string name = "Undefined";   // имя
+    public int age;                     // возраст
+ 
+    public void Print()
+    {
+        Console.WriteLine($"Имя: {name}  Возраст: {age}");
+    }
+}
+
+// =>  Visual Studio!!!!
